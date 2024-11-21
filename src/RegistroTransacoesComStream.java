e9ddc1970f33332a99340f89ba4b9bdde0a6c0bf
Miguelrespositories <danilo.miguel.pe@gmail.com>
2024-11-21 11:37:12 -0300

    FirstCommit

---
+++ src/RegistroTransacoesComStream.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class RegistroTransacoesComStream {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldo = scanner.nextDouble();
+        int quantidadeTransacoes = scanner.nextInt();
+
+        List<Transacao> transacoes = new ArrayList<>();
+
+        for (int i = 1; i <= quantidadeTransacoes; i++) {
+          
+            char tipoTransacao = scanner.next().charAt(0);
+            double valorTransacao = scanner.nextDouble();
+
+            // TODO: Criar uma nova transação e adicioná-la à lista de transações
+            Transacao transacao = new Transacao(Character.toUpperCase(tipoTransacao), valorTransacao);
+        		transacoes.add(transacao);
+
+            // Verifica e atualiza o saldo da conta com base no tipo de transação
+            if (transacao.getTipo() == 'D') {
+                saldo += valorTransacao;
+            } else if (transacao.getTipo() == 'S') {
+                saldo -= valorTransacao;
+            } else {
+                System.out.println("Opção inválida. Utilize D para depósito ou S para saque.");
+                i--; // Decrementa o índice para repetir a iteração
+            }
+        }
+
+        System.out.println("Saldo : " + saldo);
+        System.out.println("Transacoes:");
+        transacoes.stream()
+                .map(t -> Character.toLowerCase(t.getTipo())+" de "+t.getValor())
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+
+        // Fechar o scanner para evitar vazamentos de recursos
+        scanner.close();
+    }
+}
+
+class Transacao {
+    private char tipo;
+    private double valor;
+
+    public Transacao(char tipo, double valor) {
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
